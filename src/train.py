f71ea3318aaab7e4b3739837dfc9c53b6dc74039
TimJo21 <93178218+TimJo21@users.noreply.github.com>
2026-06-29 22:51:05 +0900

    src/train.py 만들기

---
+++ src/train.py
@@ -0,0 +1,16 @@
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+import os
+
+def train_model(X_train, y_train):
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+
+    os.makedirs("models", exist_ok=True)
+    joblib.dump(model, "models/iris_model.pkl")
+
+    return model
